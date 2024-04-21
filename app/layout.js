88dcbf468302e20559b5b758c1ef9e82a6e173f0
HazWahb8080 <hazem.wahb100@gmail.com>
2024-04-21 17:17:57 +0200

    fix app-router-to-be-mounted issue

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Providers>
-        <body className={inter.className}>{children}</body>
-      </Providers>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
